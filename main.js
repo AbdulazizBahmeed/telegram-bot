713c2c5e14f0d724dceec424bba219c7e562f561
abulaziz bahamid <gimo999@hotmail.com>
2023-02-26 07:49:34 +0300

    added the buttons

--- main.js
+++ main.js
@@ -1,13 +1,51 @@
-const { Telegraf } = require('telegraf');
+const { Telegraf, Markup } = require('telegraf');
 const { message } = require('telegraf/filters');
 
 // const bot = new Telegraf(process.env.BOT_TOKEN );
 const bot = new Telegraf("6165091379:AAEmRGRGTjx4eTQnOLiEgZr1PtD3zqAmIv4");
 
 bot.start((ctx) => ctx.reply('hi there'));
+
 bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+
+bot.hears('hi', (ctx) => {
+    ctx.reply('this is text', Markup
+    .keyboard([
+      ["Button 1", 'button 2'], // Row1 with 2 buttons
+      ['button 3', 'button 4'], // Row2 with 2 buttons
+      ['button 5', 'button 6', 'button 7'] // Row3 with 3 buttons
+    ])
+    .oneTime()
+    .resize()
+  )
+});
+
+bot.hears('Button 1', (ctx) => {
+    ctx.reply(ctx.message.text.substring(0,2));
+    
+})
+
+bot.hears("inline", (ctx) => {
+    ctx.reply("Hi there!", {
+        reply_markup: {
+            inline_keyboard: [
+                /* Inline buttons. 2 side-by-side */
+                [ { text: "Button 1", callback_data: "data" }, { text: "Button 2", callback_data: "btn-2" } ],
+
+                /* One button */
+                [ { text: "Next", callback_data: "next" } ],
+                
+                /* Also, we can have URL buttons. */
+                [ { text: "Open in browser", url: "telegraf.js.org" } ]
+            ]
+        }
+    });
+});
+
+bot.action('data',
+    (ctx) => ctx.reply('hello world')
+);
+
 bot.launch();
 
 // Enable graceful stop
